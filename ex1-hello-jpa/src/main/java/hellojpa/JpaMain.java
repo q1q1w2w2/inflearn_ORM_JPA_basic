7cb4ce0a11ae9cc29573c5925310b0dc9f1158ce
q1q1w2w2 <106463879+q1q1w2w2@users.noreply.github.com>
2024-07-03 21:15:11 +0900

    2024.07.03 자바 ORM 표준 JPA 기본편 시작
    영속성 컨텍스트, 엔티티 매핑

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,38 @@
+package hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("C");
+
+            em.persist(member);
+            System.out.println("member = " + member.getId());
+            System.out.println("================");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+}
